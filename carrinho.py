4216498b69a0e15423c9bf0f881f62404b2a4abf
henriq-az <caladoh8@gmail.com>
2025-05-23 23:53:46 -0300

    Update carrinho.py

--- carrinho.py
+++ carrinho.py
@@ -1,1 +1,19 @@
+class Carrinho:
+    def __init__(self):
+        self.itens = []
+
+    def adicionar_item(self, obra):
+        self.itens.append(obra)
+
+    def calcular_total(self):
+        return sum(obra.preco for obra in self.itens)
+
+    def exibir_carrinho(self):
+        print("\nCarrinho de Compras:")
+        if not self.itens:
+            print("Carrinho vazio.")
+            return
+        for obra in self.itens:
+            print(f"- {obra}")
+        print(f"Total: R${self.calcular_total():.2f}")
 
